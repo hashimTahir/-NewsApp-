83ec16defc8f05058268ed7e5a2bff457d22c91b
hashim <hashimtahir@live.com>
2020-07-29 21:08:53 +0500

    FragmentContainerview and Nav controller added
    Signed-off-by: hashim <hashimtahir@live.com>

--- app/src/main/java/com/example/hashim/newsapp/ui/NewsActivity.kt
+++ app/src/main/java/com/example/hashim/newsapp/ui/NewsActivity.kt
@@ -4,13 +4,32 @@
 
 package com.example.hashim.newsapp.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.example.hashim.newsapp.R
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class NewsActivity : AppCompatActivity() {
+
+    private var hNewNavHostFragment: NavHostFragment? = null
+    private lateinit var hNavController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        hInitNavController()
+    }
+
+    private fun hInitNavController() {
+        hNewNavHostFragment =
+            supportFragmentManager.findFragmentById(R.id.hNewsNavHostFragment) as NavHostFragment?
+        hNavController = hNewNavHostFragment!!.navController
+        hNavController?.setGraph(R.navigation.main_nav_host_navigation)
+        NavigationUI.setupWithNavController(hBottomNavigationView, hNavController)
+
     }
 }
