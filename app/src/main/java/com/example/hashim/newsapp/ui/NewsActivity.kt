975f3e96cf57b246690a6ea2efe8428e1b074069
hashim <hashimtahir@live.com>
2020-07-29 19:57:42 +0500

    fragments and navigation graph added.
    Signed-off-by: hashim <hashimtahir@live.com>

--- app/src/main/java/com/example/hashim/newsapp/ui/NewsActivity.kt
+++ app/src/main/java/com/example/hashim/newsapp/ui/NewsActivity.kt
@@ -2,12 +2,13 @@
  * Copyright (c) 2020/  7/ 29.  Created by Hashim Tahir
  */
 
-package com.example.hashim.newsapp
+package com.example.hashim.newsapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.hashim.newsapp.R
 
-class MainActivity : AppCompatActivity() {
+class NewsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ app/src/main/java/com/example/hashim/newsapp/ui/fragments/SearchNewsFragment.kt
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2020/  7/ 29.  Created by Hashim Tahir
+ */
+
+package com.example.hashim.newsapp.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.hashim.newsapp.R
+
+
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+
+class SearchNewsFragment : Fragment(R.layout.fragment_search_news) {
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            BreakingNewsFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
