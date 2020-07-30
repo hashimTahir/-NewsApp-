746767d76097f36c625834acdec3b4a499e634d2
hashim <hashimtahir@live.com>
2020-07-31 02:33:06 +0500

    Coroutines function added to get the breaking news. Handling the ResponseResource in viewmodel
    Signed-off-by: hashim <hashimtahir@live.com>

--- app/src/main/java/com/example/hashim/newsapp/repository/NewsRepository.kt
+++ app/src/main/java/com/example/hashim/newsapp/repository/NewsRepository.kt
@@ -4,10 +4,16 @@
 
 package com.example.hashim.newsapp.repository
 
+import com.example.hashim.newsapp.models.NewsResponse
+import com.example.hashim.newsapp.network.RetrofitInstance
 import com.example.hashim.newsapp.roomdb.ArticleDatabase
+import retrofit2.Response
 
 class NewsRepository(
-    val hDatabase: ArticleDatabase
+    private val hDatabase: ArticleDatabase
 ) {
+    suspend fun hGetBreakingNews(hCountryCode: String, hPageNo: Int): Response<NewsResponse> {
+        return RetrofitInstance.hNewsApi.hGetBreakingNews(hCountryCode, hPageNo)
+    }
 
 }
--- app/src/main/java/com/example/hashim/newsapp/ui/NewsViewModel.kt
+++ app/src/main/java/com/example/hashim/newsapp/ui/NewsViewModel.kt
@@ -4,11 +4,36 @@
 
 package com.example.hashim.newsapp.ui
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.hashim.newsapp.models.NewsResponse
 import com.example.hashim.newsapp.repository.NewsRepository
+import com.example.hashim.newsapp.utils.ResponseResource
+import kotlinx.coroutines.launch
+import retrofit2.Response
 
 class NewsViewModel(
     val hNewsRepository: NewsRepository
 ) : ViewModel() {
+    val hBreakingNewsMutableLiveData: MutableLiveData<ResponseResource<NewsResponse>> =
+        MutableLiveData()
+    var hPageNo = 1
 
+    fun hGetBreakingNews(hCountryCode: String) {
+        viewModelScope.launch {
+            hBreakingNewsMutableLiveData.value = ResponseResource.Loading()
+            var hBreakingNewsResponseResource =
+                hNewsRepository.hGetBreakingNews(hCountryCode, hPageNo)
+        }
+    }
+
+    private fun hHandleBreakingNewsResposne(hResponse: Response<NewsResponse>): ResponseResource<NewsResponse> {
+        if (hResponse.isSuccessful) {
+            hResponse.body()?.let { newsResponse ->
+                return ResponseResource.Success(newsResponse)
+            }
+        }
+        return ResponseResource.Error(hResponse.message())
+    }
 }
