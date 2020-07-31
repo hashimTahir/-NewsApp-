1c240180aba316b64fd4cc25106c32d0ad0b771d
hashim <hashimtahir@live.com>
2020-07-31 20:20:18 +0500

    Swipe functionaltion added in SavedNewsFragment
    Signed-off-by: hashim <hashimtahir@live.com>

--- app/src/main/java/com/example/hashim/newsapp/ui/fragments/SavedNewsFragment.kt
+++ app/src/main/java/com/example/hashim/newsapp/ui/fragments/SavedNewsFragment.kt
@@ -10,18 +10,45 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.hashim.newsapp.Constants
 import com.example.hashim.newsapp.R
 import com.example.hashim.newsapp.adapters.NewsAdapter
 import com.example.hashim.newsapp.ui.NewsViewModel
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.fragment_saved_news.*
 
 class SavedNewsFragment : Fragment(R.layout.fragment_saved_news) {
-    private var param1: String? = null
-    private var param2: String? = null
+
     private val hNewsViewModel: NewsViewModel by activityViewModels()
     private lateinit var hNewsAdapter: NewsAdapter
+    private val hItemTouchHelperCallback = object : ItemTouchHelper.SimpleCallback(
+        ItemTouchHelper.UP or ItemTouchHelper.DOWN,
+        ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+    ) {
+        override fun onMove(
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder,
+            target: RecyclerView.ViewHolder
+        ): Boolean {
+            return true
+        }
+
+        override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+            val hArticle = hNewsAdapter.hAsyncListDiffer.currentList.get(viewHolder.adapterPosition)
+            hNewsViewModel.hDeleteArticle(hArticle)
+            view?.let {
+                Snackbar.make(it, "Article Deleted", Snackbar.LENGTH_LONG).apply {
+                    setAction("Undo") {
+                        hNewsViewModel.hSaveArticle(hArticle)
+                    }
+                    show()
+                }
+            }
+        }
+    }
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -50,6 +77,9 @@ class SavedNewsFragment : Fragment(R.layout.fragment_saved_news) {
         rvSavedNews.apply {
             adapter = hNewsAdapter
             layoutManager = LinearLayoutManager(requireContext())
+            ItemTouchHelper(hItemTouchHelperCallback).apply {
+                attachToRecyclerView(rvSavedNews)
+            }
 
         }
     }
