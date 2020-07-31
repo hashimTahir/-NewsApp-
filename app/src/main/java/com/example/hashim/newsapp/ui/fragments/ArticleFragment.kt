74cf1e0fd03e22202e6fc4e958c2ac9e917b0ea1
hashim <hashimtahir@live.com>
2020-07-31 19:59:09 +0500

    Saving an Article functionality added in ArticleFragment. Viewing the Saved articles added in SavedNewsFragment and observers added.
    Signed-off-by: hashim <hashimtahir@live.com>

--- app/src/main/java/com/example/hashim/newsapp/ui/fragments/ArticleFragment.kt
+++ app/src/main/java/com/example/hashim/newsapp/ui/fragments/ArticleFragment.kt
@@ -11,13 +11,21 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.navArgs
 import com.example.hashim.newsapp.R
+import com.example.hashim.newsapp.models.Article
 import com.example.hashim.newsapp.ui.NewsViewModel
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.fragment_article.*
 
 
 class ArticleFragment : Fragment(R.layout.fragment_article) {
     private val hNewsViewModel: NewsViewModel by activityViewModels()
     private val hArguments: ArticleFragmentArgs by navArgs()
+
+    private val hArticle: Article
+        get() {
+            return hArguments.hArticle
+        }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,11 +34,20 @@ class ArticleFragment : Fragment(R.layout.fragment_article) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         hSetupView()
+        hSetupListners()
 
     }
 
+    private fun hSetupListners() {
+        fab.setOnClickListener { view ->
+            hNewsViewModel.hSaveArticle(hArticle)
+            Snackbar.make(view, "Article Saved", Snackbar.LENGTH_SHORT)
+                .show()
+        }
+    }
+
+
     private fun hSetupView() {
-        var hArticle = hArguments.hArticle
         webView.apply {
             webViewClient = WebViewClient()
             loadUrl(hArticle.url)
--- app/src/main/java/com/example/hashim/newsapp/ui/fragments/SavedNewsFragment.kt
+++ app/src/main/java/com/example/hashim/newsapp/ui/fragments/SavedNewsFragment.kt
@@ -4,17 +4,54 @@
 
 package com.example.hashim.newsapp.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.hashim.newsapp.Constants
 import com.example.hashim.newsapp.R
+import com.example.hashim.newsapp.adapters.NewsAdapter
 import com.example.hashim.newsapp.ui.NewsViewModel
-
-
-
+import kotlinx.android.synthetic.main.fragment_saved_news.*
 
 class SavedNewsFragment : Fragment(R.layout.fragment_saved_news) {
     private var param1: String? = null
     private var param2: String? = null
     private val hNewsViewModel: NewsViewModel by activityViewModels()
+    private lateinit var hNewsAdapter: NewsAdapter
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        hInitRecycler()
+        hSubscribeObservers()
+    }
+
+    private fun hSubscribeObservers() {
+        hNewsViewModel.hGetSavedNews().observe(viewLifecycleOwner, Observer { hArtilcesList ->
+            hNewsAdapter.hAsyncListDiffer.submitList(hArtilcesList)
+        })
+    }
+
+    private fun hInitRecycler() {
+        hNewsAdapter = NewsAdapter()
+        hNewsAdapter.hSetRecyclerCallBack { hArticle ->
+            val hBundle = Bundle().apply {
+                this.putSerializable(Constants.H_ARTICLE_IC, hArticle)
+            }
+            findNavController().navigate(
+                R.id.action_hSavedNewsFragment_to_hArticleFragment,
+                hBundle
+            )
+        }
+        rvSavedNews.apply {
+            adapter = hNewsAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+
+        }
+    }
 
 }
